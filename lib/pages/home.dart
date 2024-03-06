f74466ab113b963759723dfbc1487eebb3f8f5c9
zikhonamdingazwe@gmail.com <zikhonamdingazwe@gmail.com>
2024-03-06 12:22:57 +0200

    e

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -24,6 +24,7 @@ class _HomeState extends State<Home> {
           // HeaderDesktop(),
          Mobile(
           onMenuTap: (){},
+           onMenuTap: (){},
          ),
           //Skills
           Container(
