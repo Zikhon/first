ff12d75afbf7e94b4ab0474334db30d9bb5f8ba6
zikhonamdingazwe@gmail.com <zikhonamdingazwe@gmail.com>
2024-03-06 12:26:10 +0200

    m

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -18,7 +18,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: CustomColor.scaffoldBg,
-      endDrawer: ,
+      endDrawer: Drawer(),
       body: ListView(
         children: [
           //main
